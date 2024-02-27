6f64dcc8df80ccb4724e06d8b0d00f035b897248
이승연 <sylee1274@mz.co.kr>
2024-02-27 11:15:36 +0900

    Feat: Topology - region update

--- src/topology-test/topology-test.controller.ts
+++ src/topology-test/topology-test.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { UserSecurityService } from './service/user-security.service';
+import { RegionService } from './service/region.service';
 
 @Controller('topology')
 export class TopologyTestController {
-  constructor(private readonly userSeucirytService: UserSecurityService) {}
+  constructor(
+    private readonly userSeucirytService: UserSecurityService,
+    private readonly regionService: RegionService,
+  ) {}
 
   @Get('user-security')
   getUserSecurity(): any {
     return this.userSeucirytService.updateUserSecurityTemplate();
+  }
+
+  @Get('region')
+  getRegion() {
+    return this.regionService.updateRegionTemplate();
   }
 }
--- src/topology-test/topology-test.module.ts
+++ src/topology-test/topology-test.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TopologyTestController } from './topology-test.controller';
 import { UserSecurityService } from './service/user-security.service';
+import { RegionService } from './service/region.service';
 
 @Module({
   imports: [],
   controllers: [TopologyTestController],
-  providers: [UserSecurityService],
+  providers: [UserSecurityService, RegionService],
 })
 export class TopologyTestModule {}
