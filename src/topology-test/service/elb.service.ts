8f4df04c15e64eaca02a04f928fb36b423cd649a
이승연 <sylee1274@mz.co.kr>
2024-03-26 17:00:10 +0900

    Feat: Topology - ELB updating 0326

--- src/topology-test/service/elb.service.ts
+++ src/topology-test/service/elb.service.ts
@@ -3,37 +3,103 @@ import { join } from 'path';
 import * as fs from 'fs';
 import { getD2Template, writeD2Output, replaceValue } from '@/func';
 
+interface ELBs {
+  elbId: string;
+  subnets: string[];
+  elbName: string;
+  elbType: 'internet-facing' | 'internal';
+}
+
 interface Subnet {
-  name: string;
-  cidr: string;
+  subnetId: string;
+  isPublic: boolean;
+  cidrblock: string;
 }
 
 interface AZ {
-  name: string;
-  externalSubnets: Subnet[];
-  internalSubnets: Subnet[];
+  availabilityZone: string;
+  subnets: Subnet[];
 }
 
-interface ELB {
-  vpcName: string;
+interface ELBPerVPC {
+  vpcId: string;
+  internetGatewayId: string[];
   availabilityZones: AZ[];
+  elbs: ELBs[];
 }
 
 @Injectable()
 export class ELBService {
   updateELBTemplate(): void {
-    const elbJson: ELB[] = this.getELBData();
-    elbJson.forEach((elb: ELB, idx: number) => {
-      const { vpcName, availabilityZones } = elb;
+    const elbJson: ELBPerVPC[] = this.getELBData();
+    const d2Template = getD2Template('elb');
+    const azWithSubnets = [];
+    elbJson.forEach((elbPerVPC: ELBPerVPC, idx: number) => {
+      // update-vars
+      const vars = this.updateVars(elbPerVPC);
+      console.log(vars);
 
+      const { vpcId, internetGatewayId, availabilityZones, elbs }: ELBPerVPC =
+        elbPerVPC;
+
+      // az 생성
+      // TODO: 이 토폴로지 특성상 az는 두개까지만 존재한다고 가정
+      availabilityZones.forEach((az: AZ, azIdx: number) => {
+        // console.log(az['availabilityZone'], idx);
+        if (az['subnets']) {
+          if (az['subnets'].length > 0) {
+            azWithSubnets.push(az);
+            // console.log(primaryAZ);
+            // const secondaryAZ =
+          }
+        }
+      });
+
+      // az내 subnet 업데이트
       availabilityZones.forEach((az: AZ, idx: number) => {
-        let updatedVars = this.updateVars(az); // subnet
-        updatedVars += `  vpc-name: ${vpcName} \n`; // vpcName
-        updatedVars += `  az-name${idx + 1}: ${az['name']} \n`; // azName
-        // console.log(updatedVars, idx);
+        // console.log(subnets, az['availabilityZone']);
         // console.log('=====================================');
+
+        if (idx === 0) {
+          // update-primary-subnet
+          const primarySubnets = this.updateSubnets(az);
+          // console.log('primarySubnets= ', primarySubnets);
+        } else if (idx === 1) {
+          // update-secondary-subnet
+          const secondarySubnets = this.updateSubnets(az);
+          // console.log('secondarySubnets= ', secondarySubnets);
+        }
+      });
+
+      elbs.forEach((elb: ELBs, idx: number) => {
+        let isExternal = '';
+        switch (elb['elbType']) {
+          case 'internet-facing':
+            isExternal += `Internet -> VPC.Box-ELB.elb-external`;
+            break;
+          case 'internal':
+            break;
+          default:
+            break;
+        }
+
+        // subnet-internal: extra
       });
     });
+
+    if (azWithSubnets.length > 1) {
+      // update-primary-az
+      azWithSubnets.forEach((az: AZ, azIdx: number) => {
+        const primaryAZ = az['availabilityZone'];
+        const updatedPrimaryAZ = replaceValue(
+          d2Template,
+          '{{update-primary-az}}',
+          primaryAZ,
+        );
+
+        // console.log(updatedPrimaryAZ);
+      });
+    }
 
     // update-[primary/secondary]-subnet
 
@@ -53,24 +119,106 @@ export class ELBService {
     }
   }
 
-  private updateVars(az: AZ) {
-    let vars = '';
+  private updateVars(elbPerVPc: ELBPerVPC) {
+    let result = '';
+    // elb-external n개
+    // elb-internal n개
+    elbPerVPc['elbs'].forEach((elb: ELBs, elbIdx: number) => {
+      let elbExternal = '';
+      let elbInternal = '';
 
-    az['externalSubnets'].forEach((subnet: Subnet, subExNum: number) => {
-      vars += `  subnet-external${subExNum + 1}: ${subnet['cidr']} \n`;
+      if (elb['elbType'] === 'internet-facing') {
+        elbExternal += `    elb-external${elbIdx + 1}.class: elb-external \n`;
+        elbExternal += `    elb-external${elbIdx + 1}: "" { \n`;
+        elbExternal += `      label: ${elb['elbName']} \n`;
+        elbExternal += `    }\n`;
+
+        result += elbExternal;
+      } else if (elb['elbType'] === 'internal') {
+        elbInternal += `    elb-internal${elbIdx + 1}.class: elb-internal \n`;
+        elbInternal += `    elb-internal${elbIdx + 1}: "" { \n`;
+        elbInternal += `      label: ${elb['elbName']} \n`;
+        elbInternal += `    }\n`;
+
+        result += elbInternal;
+      }
     });
 
-    az['internalSubnets'].forEach((subnet: Subnet, subInNum: number) => {
-      vars += `  subnet-internal${subInNum + 1}: ${subnet['cidr']} \n`;
+    // availabilityZones 2개 고정.
+    elbPerVPc['availabilityZones'].forEach((az: AZ, azIdx: number) => {
+      let azName = '';
+      if (az['subnets']) {
+        if (az['subnets'].length > 0) {
+          azName += `  az-name${azIdx + 1}.class: box \n`;
+
+          result += azName;
+        }
+      }
     });
-    return vars;
+
+    // vpc-name
+    result += `  vpc-id: ${elbPerVPc['vpcId']} \n`;
+    return result;
   }
 
-  private updateSubnet(az: AZ) {
-    // subnet-external
-    let externalSubnet = '';
-    az['externalSubnets'].forEach((subnet: Subnet, subExNum: number) => {
-      externalSubnet += `  subnet-external${subExNum + 1} \n`;
+  // private updatePrimaryAZ(az: AZ) {
+  //   let primaryAZ = '';
+
+  //   primaryAZ += `  AZ-PRIMARY.class: box \n`;
+  //   primaryAZ += `  AZ-PRIMARY: "" { \n`;
+  //   primaryAZ += `    grid-columns: 1 \n`;
+  //   primaryAZ += `    grid-gap: 20 \n`;
+  //   primaryAZ += `    label: ${az['availabilityZone']} \n \n`;
+  //   primaryAZ += `    "{{update-primary-subnet}}" \n \n`;
+  //   primaryAZ += `  }\n`;
+  //   // subnet
+  //   // az['subnets'].forEach((subnet: Subnet, idx: number) => {
+  //   //   if (idx < 2) {
+  //   //     primaryAZ += `    subnet${idx + 1}.class: subnet \n`;
+  //   //     primaryAZ += `    subnet${idx + 1}: "" { \n`;
+  //   //     primaryAZ += `      label: ${subnet['cidrblock']} \n`;
+  //   //     primaryAZ += `    }\n`;
+  //   //   }
+  //   // });
+
+  //   return primaryAZ;
+  // }
+
+  private updateSubnets(az: AZ) {
+    // az별 subnet 업데이트
+    let subnets = '';
+
+    if (Array.isArray(az['subnets'])) {
+      az['subnets'].forEach((subnet: Subnet, idx: number) => {
+        subnets += `    subnet${idx + 1}. class: subnet \n`;
+        subnets += `    subnet${idx + 1}: "" { \n`;
+        subnets += `      label: ${subnet['cidrblock']} \n`;
+        subnets += `    }\n`;
+      });
+    }
+
+    return subnets;
+  }
+
+  // private updateExternalELB(elb: ELBs) {}
+
+  private updateInternalELB(elb: ELBs) {
+    let internalELB = '';
+
+    elb['subnets'].forEach((subnet: string, idx: number) => {
+      internalELB += `    subnet-internal${idx + 1}. class: subnet-internal \n`;
+      internalELB += `    subnet-internal${idx + 1}: "" { \n`;
+      internalELB += `      label: ${subnet} \n`;
+      internalELB += `    }\n`;
     });
+
+    return internalELB;
+  }
+
+  private updateLinks(az: AZ) {
+    // update-links-[primary/secondary]
+    // let primaryLinks = '';
+    // let secondaryLinks = '';
+    // az[''];
   }
 }
